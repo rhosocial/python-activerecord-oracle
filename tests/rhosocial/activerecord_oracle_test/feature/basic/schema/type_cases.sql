629acf9e9e588b24f9fb4b527177093ccc0dc558
zhao vistart <vistart@rho.social>
2026-05-09 01:30:25 +0800

    fix(oracle): use LOB (...) STORE AS BASICFILE syntax for SYSTEM tablespace

--- tests/rhosocial/activerecord_oracle_test/feature/basic/schema/type_cases.sql
+++ tests/rhosocial/activerecord_oracle_test/feature/basic/schema/type_cases.sql
@@ -12,12 +12,12 @@ CREATE TABLE type_cases (
     decimal_val NUMBER(20,4),
     char_val CHAR(10),
     varchar_val VARCHAR2(4000),
-    text_val CLOB BASICFILE,
+    text_val CLOB,
     date_val DATE,
     time_val VARCHAR2(20),
     timestamp_val TIMESTAMP WITH TIME ZONE,
-    blob_val BLOB BASICFILE,
+    blob_val BLOB,
     json_val JSON,
     array_val JSON,
     is_active NUMBER(1)
-)
+) LOB (text_val, blob_val) STORE AS BASICFILE
