rhosocial/python-activerecord-firebird

test(basic): add bulk operations schema and provider
Co-Authored-By: Claude Opus 4.7 <noreply@anthropic.com>

tests/rhosocial/activerecord_firebird_test/feature/basic/schema/bulk_users.sql
@@ -1,0 +1,6 @@
+CREATE TABLE bulk_users (
+    id INT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT DEFAULT 0,
+    email VARCHAR(255) DEFAULT ''
+);

rhosocial/python-activerecord-oracle

tests/rhosocial/activerecord_oracle_test/feature/basic/schema/bulk_users.sql
@@ -1,0 +1,6 @@
+CREATE TABLE bulk_users (
+    id NUMBER GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+    name VARCHAR2(255) NOT NULL,
+    age NUMBER DEFAULT 0,
+    email VARCHAR2(255) DEFAULT ''
+)